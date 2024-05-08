59786da323ae5ba8c216f1da6c3b3712de4d3043
phc31117 <phc31117@gmail.com>
2024-05-09 00:58:46 +0800

    update the code

--- Add Gaussian Noise/add_gaussian_noise.py
+++ Add Gaussian Noise/add_gaussian_noise.py
@@ -3,7 +3,7 @@ python version : 3.8.8
 opencv version : 4.8.1
 
 target:
-generate 50 variations with sigma = 35
+generate 50 variations with sigma = 35 as noise, then add the noise into the picture
 np.random.normal(mean, sigma, (H, W, C)) # mean = 0, sigma = 35
 np.clip(img, a_min, a_max) # limit the bound
 
@@ -13,27 +13,41 @@ photo credit : Photo by Biel Morro on Unsplash
 
 import cv2
 import numpy as np
-import random
+import matplotlib.pyplot as plt
 
 img = cv2.imread('pics/pic1.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# print(img.shape) # (5730, 3810, 3)
 
-# [H, W, C] : height, width, channel
-# print(img.shape)  # (853, 1280, 3) 
-
-
+# add noise
 final = img
-
 for i in range(50): # add 50 times
     noise = np.random.normal(0, 35, img.shape)
     img_add = np.add(img, noise)
     img_add = np.clip(img_add, 0, 255).astype(np.uint8) # limit the upper & lower bound
     final = np.mean(np.array([img_add, final]), axis = 0).astype(np.uint8)
-    
 
+diffrence = final - img
 
-cv2.imshow('pic1', final)
-cv2.waitKey(0)                 # press any key to stop the window
-cv2.destroyAllWindows()        # close the window
+# Initial Image
+plt.figure(figsize=(img.shape[0]*0.005,img.shape[1]*0.005))
+plt.subplot(1,3,1)
+plt.axis("off")
+plt.title("Before (Initial Image)")
+plt.imshow(img)
 
-# 存下圖片檔
-cv2.imwrite('pics/pic1_add_gaussian_noise.jpg', final)
+# Image with Gaussian Noise
+plt.subplot(1,3,2)
+plt.axis("off")
+plt.title("After (With Gaissian Noise)")
+plt.imshow(final)
+
+# Difference
+plt.subplot(1,3,3)
+plt.axis("off")
+plt.title("Difference")
+plt.imshow(diffrence)
+
+# save the result
+plt.savefig("pics/gaussian_noise_result.jpg")
+plt.show()
